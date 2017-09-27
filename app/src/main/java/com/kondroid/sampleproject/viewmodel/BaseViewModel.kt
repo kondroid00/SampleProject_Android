60bfeabe52cf5a9734bcfe96c19daee643fa86c9
Koichiro Kondo <kondo@kondo.local>
2017-09-28 04:25:23 +0900

    DataBindingのview作成

---
+++ app/src/main/java/com/kondroid/sampleproject/viewmodel/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.kondroid.sampleproject.viewmodel
+
+/**
+ * Created by kondo on 2017/09/28.
+ */
+
+open class BaseViewModel {
+
+}
---
+++ app/src/main/java/com/kondroid/sampleproject/viewmodel/TopViewModel.kt
@@ -0,0 +1,9 @@
+package com.kondroid.sampleproject.viewmodel
+
+/**
+ * Created by kondo on 2017/09/28.
+ */
+
+class TopViewModel : BaseViewModel() {
+
+}
