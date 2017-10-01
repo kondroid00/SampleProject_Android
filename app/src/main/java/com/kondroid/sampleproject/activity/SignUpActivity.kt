4a9d6dfec434fbc5f4c427d2209db9b41941dd9b
Koichiro Kondo <kondo@kondo.local>
2017-10-02 03:02:54 +0900

    SignUpActivity追加

---
+++ app/src/main/java/com/kondroid/sampleproject/activity/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.kondroid.sampleproject.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kondroid.sampleproject.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
---
+++ app/src/main/java/com/kondroid/sampleproject/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,13 @@
+package com.kondroid.sampleproject.viewmodel
+
+/**
+ * Created by kondo on 2017/09/28.
+ */
+
+class SignUpViewModel : BaseViewModel() {
+
+
+    fun tapRegister() {
+
+    }
+}
