a7f78f1a27153e4fdd6d40dbc60a2fccc5544f4c
Koichiro Kondo <kondo@kondo.local>
2017-10-19 20:29:48 +0900

    レイアウト作成

---
+++ app/src/main/java/com/kondroid/sampleproject/viewmodel/ChatViewModel.kt
@@ -0,0 +1,14 @@
+package com.kondroid.sampleproject.viewmodel
+
+import android.content.Context
+
+/**
+ * Created by kondo on 2017/10/19.
+ */
+
+class ChatViewModel(context: Context) : BaseViewModel(context) {
+
+    override fun initVM() {
+        super.initVM()
+    }
+}
